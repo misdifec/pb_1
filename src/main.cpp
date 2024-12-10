296f3151f477bc4cb62f69a74a8608742051d090
Elisaveta Tereknina <lizavetaterehina@gmail.com>
2024-12-10 11:22:46 +0300

    Pole 1

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,56 @@
 void KarelProgram() {
 /// @todo Добавить программу для Карела
 
+TurnLeft();
+while(FrontIsClear() == true){
+TurnLeft();
+TurnLeft();
+TurnLeft();
+    while( FrontIsClear() == true){
+  if (BeepersPresent() == true){
+    PickBeeper();
 }
+  if(BeepersPresent() == false and FrontIsClear() == true){
+    Move();
+}
+}
+while (BeepersPresent() == true){
+    PickBeeper();
+}
+TurnLeft();
+Move();
+TurnLeft();
+while(FrontIsClear() == true){
+    Move();
+}
+TurnLeft();
+TurnLeft();
+TurnLeft();
+}
+
+TurnLeft();
+TurnLeft();
+TurnLeft();
+    while( FrontIsClear() == true){
+  if (BeepersPresent() == true){
+    PickBeeper();
+}
+  if (BeepersPresent() == false and FrontIsClear() == true){
+    Move();
+}
+}
+while(BeepersPresent() == true){
+    PickBeeper();
+}
+}
+
 
 
 //==============================================================================
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/collect_newspaper.w");
+    LoadWorld("worlds/CB_01.w");
     KarelProgram();
     Finish();
     return 0;
